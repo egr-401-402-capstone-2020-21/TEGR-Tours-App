61f37a71edfaab01ef99bc32d38de93494f0fd1e
Capstone <dev@ubuntu.attlocal.net>
2020-11-09 23:58:01 +0000

    auto-commit

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,28 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+  
+  def new
+  end
+
+  def create
+	@user = User.find_by(email: params[:email])
+	if @user && @user.authenticate(params[:password])
+		session[:user_id] = @user.id
+		redirect_to '/welcome'
+	else
+		redirect_to '/login'
+	end
+  end
+
+  def destroy
+  end
+
+  def login
+  end
+	
+  def welcome
+  end
+
+  def page_requires_login
+  end
+end
