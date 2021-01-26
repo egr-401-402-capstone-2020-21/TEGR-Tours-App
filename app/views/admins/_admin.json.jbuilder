dff1959eaf8290134a156aa34a252f87b3c8997b
Nick Mark <35472105+NickMarkG@users.noreply.github.com>
2021-01-25 16:36:36 -0800

    Revert "auto-commit"

--- app/views/admins/_admin.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! admin, :id, :adminname, :created_at, :updated_at
-json.url admin_url(admin, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  resources :admins
   root 'static#welcome'
   resources :time_blocks
   resources :courses
   resources :rooms
-
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'signup', to: 'users#new'
-  get 'login', to: 'admin_sessions#new' 
-  post 'login', to: 'admin_sessions#create'
-  get 'logout', to: 'admin_sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
