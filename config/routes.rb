dd36e85f1ef179a08b841200c23233482e097f40
Capstone <dev@ubuntu.attlocal.net>
2021-02-06 14:47:49 -0800

    Login/Register

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static#welcome'
   resources :time_blocks
   resources :courses
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_032214) do
+ActiveRecord::Schema.define(version: 2021_02_06_223508) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -52,6 +52,19 @@ ActiveRecord::Schema.define(version: 2020_11_09_032214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_time_blocks_on_course_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
