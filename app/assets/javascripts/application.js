e72c676de71affd08465538c9aee38ab2ed70fa0
Capstone <dev@ubuntu.attlocal.net>
2021-02-06 16:04:13 -0800

    rooms_with_modals

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,19 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).on('turbolinks:load', function() {
+	$('#roomModal').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget) // Button that triggered the modal
+	  var code = button.data('code') // Extract info from data-* attributes
+	  var title = button.data('title') // Extract info from data-* attributes
+	  var desc = button.data('description') // Extract info from data-* attributes
+	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	  var modal = $(this)
+	  modal.find('.modal-title').text(code)
+	  modal.find('.modal-body input').val(title)
+	  modal.find('.modal-body textarea').val(desc)
+	})
+})
