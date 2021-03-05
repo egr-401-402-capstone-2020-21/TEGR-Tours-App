03dbed6f78a876df8c1c6033f5f46afba5bb3957
Capstone <dev@ubuntu.attlocal.net>
2021-03-05 15:25:34 -0800

    switch_qr_codes_to_png

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -23,4 +23,17 @@ class ApplicationRecord < ActiveRecord::Base
 	def svg_path(record)
 		"#{TegrQR::QR_PATH}/#{record.class.name}_#{record.id}.svg"
 	end
+
+	def save_png(png, record)
+		Rails.logger.info "*** Attempting to save file ***"
+		IO.binwrite(png_path(record), png.to_s)
+	end
+
+	def delete_png(record)
+		File.delete(png_path(record)) if File.exist?(png_path(record))
+	end
+
+	def png_path(record)
+		"#{TegrQR::QR_PATH}/#{record.class.name}_#{record.id}.png"
+	end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -13,18 +13,33 @@ class Room < ApplicationRecord
 		qrcode = RQRCode::QRCode.new(TegrQR::DOMAIN + room_path(self))
 
 		# NOTE: showing with default options specified explicitly
-		svg = qrcode.as_svg(
-			offset: 0,
-			color: '000',
-			shape_rendering: 'crispEdges',
-			module_size: 6,
-			standalone: true)
+		#svg = qrcode.as_svg(
+		#	offset: 0,
+		#	color: '000',
+		#	shape_rendering: 'crispEdges',
+		#	module_size: 6,
+		#	standalone: true)
 
-		save_svg(svg, self)
+		#save_svg(svg, self)
+
+		png = qrcode.as_png(
+		  bit_depth: 1,
+		  border_modules: 4,
+		  color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+		  color: 'black',
+		  file: nil,
+		  fill: 'white',
+		  module_px_size: 6,
+		  resize_exactly_to: false,
+		  resize_gte_to: false,
+		  size: 120
+		)
+
+		save_png(png, self)
 	end
 
 
 	def destroy_qr_code
-		delete_svg(self)
+		delete_png(self)
 	end
 end
