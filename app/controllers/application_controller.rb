ccc72ddb0a6d63449e529154965bb002da231d6e
Capstone <dev@ubuntu.attlocal.net>
2021-04-12 17:17:14 -0700

    auto-commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-	module TegrQR
-		DOMAIN = "http://tegrtours.com"
-		QR_PATH = "#{Rails.root}/app/assets/images/qr_codes"
-	end
-
-	def save_svg(svg, room_id)
-		file_path = "#{TegrQR::QR_PATH}/room_#{room_id}.svg"
-
-		aFile = File.new(file_path, "w")
-		if aFile
-		   aFile.syswrite(svg)
-		else
-		   Rails.logger.info "Unable to open file!"
-		end
-	end
 end
