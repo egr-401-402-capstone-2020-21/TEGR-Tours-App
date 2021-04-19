085fa1d44523f66572f40bcfd4867ee8f85b86ac
sprado41 <31782900+sprado41@users.noreply.github.com>
2021-04-19 16:17:29 -0700

    Configured for Docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,21 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'http://tegrtours.com', port: 80 }
+  config.action_mailer.default_options = { from: 'tegrvirtualtours@gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'mail.google.com',
+    :authentication => 'plain',
+    :user_name => ENV["EMAIL_USERNAME"],
+    :password => ENV["EMAIL_PASSWORD"]
+  }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
