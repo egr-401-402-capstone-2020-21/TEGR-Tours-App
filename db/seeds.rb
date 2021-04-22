5538dad1c8ee51ec9cb167518379ab60df303003
Abanoub Farag <42988426+abanoub66@users.noreply.github.com>
2021-04-22 15:08:13 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(:admin => true, :email => 'lclement@calbaptist.edu', :password => 'helloworld', :password_confirmation => 'helloworld'))
